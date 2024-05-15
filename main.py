63449ffdee739e4b48350bc855ddc76fa13fa17d
airguy <airguy-one@yandex.ru>
2024-05-15 11:31:49 +0300

    some

--- main.py
+++ main.py
@@ -1,30 +1,47 @@
-from blockchain.block import Block
-from blockchain.chain import Blockchain
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage
+from aiogram_tonconnect.handlers import AiogramTonConnectHandlers
+from aiogram_tonconnect.middleware import AiogramTonConnectMiddleware
 
-blockchain = Blockchain()
+BOT_TOKEN = "1234567890:QWERTYUIOPASDFGHJKLZXCVBNM"
 
-# Create a new block
-block1 = Block(
-    index=1,
-    timestamp="02/01/2022",
-    transactions=[{"sender": "Alice", "recipient": "Bob", "amount": 10}],
-    previous_hash="0",
-)
-blockchain.add_block(block1)
+REDIS_DSN = "redis://localhost:6379/0"
 
-# Create another block
-block2 = Block(
-    index=2,
-    timestamp="03/01/2022",
-    transactions=[{"sender": "Bob", "recipient": "Charlie", "amount": 5}],
-    previous_hash="0",
-)
-blockchain.add_block(block2)
+# Link to your created manifest.json
+MANIFEST_URL = "https://raw.githubusercontent.com/nessshon/aiogram-tonconnect/main/tonconnect-manifest.json"
 
-# Print the blockchain
-for block in blockchain.chain:
-    print(f"Block #{block.index}")
-    print(f"Timestamp: {block.timestamp}")
-    print(f"Transactions: {block.transactions}")
-    print(f"Previous Hash: {block.previous_hash}")
-    print(f"Hash: {block.hash}\n")
+# List of wallets to exclude
+EXCLUDE_WALLETS = ["mytonwallet"]
+
+
+async def main():
+    # Initializing the storage for FSM (Finite State Machine)
+    storage = RedisStorage.from_url(REDIS_DSN)
+
+    # Creating a bot object with the token and HTML parsing mode
+    bot = Bot(BOT_TOKEN, parse_mode="HTML")
+
+    # Creating a dispatcher object using the specified storage
+    dp = Dispatcher(storage=storage)
+
+    # Registering middleware for TON Connect processing
+    dp.update.middleware.register(
+        AiogramTonConnectMiddleware(
+            redis=storage.redis,
+            manifest_url=MANIFEST_URL,
+            exclude_wallets=EXCLUDE_WALLETS,
+            qrcode_type="url",  # or "bytes" if you prefer to generate QR codes locally.
+        )
+    )
+
+    # Registering TON Connect handlers
+    AiogramTonConnectHandlers().register(dp)
+
+    # Starting the bot using long polling
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    import asyncio
+
+    asyncio.run(main())

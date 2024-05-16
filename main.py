ce76e3efb5058b432f0527045655069efe67d57f
airguy <airguy-one@yandex.ru>
2024-05-16 16:01:47 +0300

    some

--- main.py
+++ main.py
@@ -7,21 +7,16 @@ BOT_TOKEN = "1234567890:QWERTYUIOPASDFGHJKLZXCVBNM"
 
 REDIS_DSN = "redis://localhost:6379/0"
 
-# Link to your created manifest.json
 MANIFEST_URL = "https://raw.githubusercontent.com/nessshon/aiogram-tonconnect/main/tonconnect-manifest.json"
 
-# List of wallets to exclude
 EXCLUDE_WALLETS = ["mytonwallet"]
 
 
 async def main():
-    # Initializing the storage for FSM (Finite State Machine)
     storage = RedisStorage.from_url(REDIS_DSN)
 
-    # Creating a bot object with the token and HTML parsing mode
     bot = Bot(BOT_TOKEN, parse_mode="HTML")
 
-    # Creating a dispatcher object using the specified storage
     dp = Dispatcher(storage=storage)
 
     # Registering middleware for TON Connect processing
@@ -37,7 +32,6 @@ async def main():
     # Registering TON Connect handlers
     AiogramTonConnectHandlers().register(dp)
 
-    # Starting the bot using long polling
     await dp.start_polling(bot)
 
 
